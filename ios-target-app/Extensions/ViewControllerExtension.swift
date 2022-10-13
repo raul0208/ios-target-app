88c127c9817f6900b1d2040a7663ab579d68fb88
Raul Pineres <raul0208@gmail.com>
2022-10-13 19:59:13 +0200

    Add map and navigation bar to home

---
+++ ios-target-app/Extensions/ViewControllerExtension.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerExtension.swift
+//  ios-target-app
+//
+//  Created by Raul Piñeres Carrera on 12/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func setDefaultScreenConfigs() {
+      view.backgroundColor = .white
+    }
+    
+}
--- ios-target-app/Home/Views/HomeViewController.swift
+++ ios-target-app/Home/Views/HomeViewController.swift
@@ -6,13 +6,101 @@
 //
 
 import UIKit
+import MapKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+  
+  private lazy var mapView: MKMapView = {
+    let map = MKMapView()
+    map.overrideUserInterfaceStyle = .dark
+    return map
+  }()
+  
+  private var locationManager: CLLocationManager!
+  private var currentLocation: CLLocation?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // TODO: remove temporary implementation of background color
-    view.backgroundColor = .lightGray
+    setDefaultScreenConfigs()
+    setNavigationBar()
+    setMapConstraints()
+    setMapConfig()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: animated)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    mapView.showsUserLocation = true
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    mapView.showsUserLocation = false
+  }
+  
+  private func setNavigationBar() {
+    navigationItem.title = "home_title".localized
+    
+    let profileButtonIcon = UIBarButtonItem(
+      image: UIImage(named: "ic_home_profile"),
+      style: .plain,
+      target: self,
+      action: nil
+    )
+    
+    let chatButtonIcon = UIBarButtonItem(
+      image: UIImage(named: "ic_home_chat"),
+      style: .plain,
+      target: self,
+      action: nil
+    )
+    
+    profileButtonIcon.tintColor = .black
+    chatButtonIcon.tintColor = .black
+    
+    navigationItem.leftBarButtonItem = profileButtonIcon
+    navigationItem.rightBarButtonItem = chatButtonIcon
+  }
+  
+  private func setMapConfig() {
+    mapView.delegate = self
+    
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    
+    // Check for Location Services
+    if CLLocationManager.locationServicesEnabled() {
+      locationManager.requestWhenInUseAuthorization()
+      locationManager.startUpdatingLocation()
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    defer { currentLocation = locations.last }
+    
+    if currentLocation == nil {
+      // Zoom to user location
+      if let userLocation = locations.last {
+        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.setRegion(viewRegion, animated: false)
+      }
+    }
+  }
+  
+  private func setMapConstraints() {
+    view.addSubview(mapView)
+    
+    mapView.translatesAutoresizingMaskIntoConstraints = false
+    
+    mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+    mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
   }
 }
